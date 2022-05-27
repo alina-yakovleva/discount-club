5755dbb27464a813e428e49f06882b59f0773da1
Alina Yakovleva <alinayakovleva446@gmail.com>
2022-05-27 16:00:36 +0300

    добавлена переменная токен из файла .env

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -1,9 +1,11 @@
 import Axios from "axios";
 
+const TOKEN = process.env.REACT_APP_TOKEN || "";
+
 export const axios = Axios.create({
   timeout: 1000,
   headers: {
     accept: "application/json",
-    Authorization: "435b7c13-ecaf-4265-83c8-186cca3242cc",
+    Authorization: TOKEN,
   },
 });
